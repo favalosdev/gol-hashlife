240d18504594ab20ddcc8ab3e25679f3db1c4707
Fernando Avalos <fernandoadev@protonmail.com>
2025-10-20 22:28:43 -0500

    Minor

--- src/gol/quad_tree.rs
+++ src/gol/quad_tree.rs
@@ -32,7 +32,7 @@ pub fn join(a: Node, b: Node, c: Node, d: Node) -> Node {
 #[memoize]
 pub fn get_zero(k: usize) -> Node {
     if k == 0 {
-        // I wished there was a way to express this more succintly
+        // Ugly as fuck but what else could be done?
         Node::new(0, None, None, None, None, 0)
     } else {
         join(get_zero(k-1), get_zero(k-1), get_zero(k-1), get_zero(k-1))
